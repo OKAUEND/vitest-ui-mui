f41f37a0da6ef68c7ce5494edb7bac3d9a08eba8
OKAUEND <def1f23sbb@gmail.com>
2024-09-22 20:06:15 +0900

    変更 - テストを構築する

---
+++ src/feature/mui/mui.test.tsx
@@ -0,0 +1,15 @@
+import {describe, test,expect,} from "vitest"
+import { render,screen} from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import '@testing-library/jest-dom'
+
+import {Mui} from "./mui"
+
+describe("Material UI", () => {
+    test("入力内容が不正の場合にエラーメッセージが表示される",  async () => {
+        render(<Mui />)
+        const input =  screen.getByRole("textbox")
+        await userEvent.type(input, "a")
+        expect(screen.getByText("入力内容が不正です")).toBeInTheDocument()
+    })
+})
